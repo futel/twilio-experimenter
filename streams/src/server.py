f68089e0b43fdad1b54955647e3fe1f17d0d0f98
Karl Anderson <kra@monkey.org>
2023-04-09 10:40:11 -0700

    rename instances

--- streams/src/server.py
+++ streams/src/server.py
@@ -16,42 +16,40 @@ import websocketserver
 #     with open(filename, "ab") as f:
 #         f.write(chunk)
 
-async def websocket_server_to_transcriber(websocket_server, transcriber):
-    async for chunk in websocket_server.receive_media():
+async def websocket_to_transcriber(websocket_server, transcriber):
+    async for chunk in websocket_server.receive_response():
         transcriber.add_request(chunk)
 
 async def transcriber_to_speaker(transcriber, speaker):
-    async for string in transcriber.receive_transcriptions():
+    async for string in transcriber.receive_response():
         speaker.add_request(string)
 
-async def speaker_to_websocket_server(speaker, websocket_server):
-    async for chunk in speaker.receive_media():
-        util.log("speaker receive media")
-        websocket_server.add_request(chunk)
+async def speaker_to_websocket(speaker, websocket):
+    async for chunk in speaker.receive_response():
+        websocket.add_request(chunk)
 
 async def main():
-    websocket_server = websocketserver.Server()
-    transcriber = transcription.SpeechClientBridge(
-        transcription.streaming_config)
     speaker = texttospeech.Client()
+    transcriber = transcription.SpeechClientBridge()
+    websocket = websocketserver.Server()
 
-    speaker_to_websocket_server_task = asyncio.create_task(
-        speaker_to_websocket_server(speaker, websocket_server))
+    speaker_to_websocket_task = asyncio.create_task(
+        speaker_to_websocket(speaker, websocket))
     speaker_task = asyncio.create_task(speaker.start())
 
     transcriber_to_speaker_task = asyncio.create_task(
         transcriber_to_speaker(transcriber, speaker))
     transcriber_task = asyncio.create_task(transcriber.start())
 
-    websocket_server_to_transcriber_task = asyncio.create_task(
-        websocket_server_to_transcriber(websocket_server, transcriber))
-    websocket_server_task = asyncio.create_task(websocket_server.start())
+    websocket_to_transcriber_task = asyncio.create_task(
+        websocket_to_transcriber(websocket, transcriber))
+    websocket_task = asyncio.create_task(websocket.start())
 
     # We should gather these if we want to be able to shut down or cancel.
-    await websocket_server_task
-    await websocket_server_to_transcriber_task
+    await websocket_task
+    await websocket_to_transcriber_task
     await transcriber_to_speaker_task
     await speaker_task
-    await speaker_to_websocket_server_task
+    await speaker_to_websocket_task
 
 asyncio.run(main())
